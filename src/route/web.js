af37ce39435afc462a20c6c8b55b0f115cc63326
phamanhquoc2809 <caheocaduoi@gmail.com>
2022-05-14 23:09:39 +0700

    tai len thu

---
+++ src/route/web.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+let router = express.Router();
+
+let initWebRouter = (app) =>{
+    router.get('/', (req, res) =>{
+        return res.send('Hello');
+    });
+    
+    
+    
+    return app.use("/", router);
+}
+
+module.exports = initWebRouter;
