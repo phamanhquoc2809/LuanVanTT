30fb1798eca1fc8a5744c4dfb5202d5ea70b34b2
phamanhquoc2809 <caheocaduoi@gmail.com>
2022-05-15 23:06:26 +0700

    done add, delete, update for user

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -18,7 +18,52 @@ let getDetailPage = async (req, res) => {
   return res.send(JSON.stringify(user));
 };
 
+let addNewUser = async (req, res) => {
+  let { firstname, lastname, email, phone, password, address, gender, id_permission } = req.body;
+  await pool.execute("insert into user(firstname, lastname, email, phone, password, address, gender, id_permission) values (?, ?, ?, ?, ?, ?, ?, ?)",
+    [firstname, lastname, email, phone, password, address, gender, id_permission]);
+
+  return res.send("Add Success");
+  // return res.status(200).json(message);
+};
+
+
+
+let addNewCate = async (req, res) => {
+  let { category } = req.body;
+
+  await pool.execute("insert into category(category) values (?)", [category]);
+  return res.send("Add Success");
+};
+let updateUser = async (req, res) => {
+  let { firstname, lastname, email, phone, password, address, gender, id_permission, id } = req.body;
+
+  if (!firstname || !lastname || !email || !phone || !password || !address || !gender || !id_permission || !id) {
+    return res.status(200).json({
+      message: 'missing required params'
+    })
+  }
+
+  await pool.execute("update user set firstname= ?, lastname=?, email=?, phone=?, password=?, address=?, gender=?, id_permission=? where id=?",
+    [firstname, lastname, email, phone, password, address, gender, id_permission, id]);
+
+  return res.send("Add Success");
+};
+let deleteUser = async (req, res) => {
+  let userId = req.params.id;
+  if (!userId) {
+    return res.status(200).json({
+      message: 'missing required params'
+    })
+  }
+  await pool.execute("delete from user where id =?", [userId]);
+  return res.send("Delete Success");
+}
 module.exports = {
   getAllUsers,
   getDetailPage,
+  addNewUser,
+  addNewCate,
+  updateUser,
+  deleteUser
 };
--- src/route/api.js
+++ src/route/api.js
@@ -4,8 +4,20 @@ import apiController from "../controllers/apiController";
 let router = express.Router();
 
 let initAPIRoute = (app) => {
+
+  // User
   router.get("/user", apiController.getAllUsers); // method GET -> READ data
   router.get("/user/:id", apiController.getDetailPage);
+
+
+  router.post("/add-new-user", apiController.addNewUser);
+
+
+  router.put("/update-user", apiController.updateUser);
+  router.delete("/delete-user/:id", apiController.deleteUser);
+
+  // Category
+  router.post("/add-new-cate", apiController.addNewCate);
 
   // router.post('/create-user', APIController.createNewUser); // method POST -> CREATE data
   // router.put('/update-user', APIController.updateUser); //method PUT -> UPDATE data
